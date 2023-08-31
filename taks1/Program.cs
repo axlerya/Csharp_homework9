d0f462e93902130ef60fb4363e7a89fe0802a464
axlerya <white_cat_02@mail.ru>
2023-08-31 21:05:25 +0500

    feat: add task 1
    add a solution to the first task

---
+++ taks1/Program.cs
@@ -0,0 +1,10 @@
+﻿Console.WriteLine("Введите число");
+int number = Convert.ToInt32(Console.ReadLine());
+
+string SequenceOfNumbers(int n)
+{
+    if (n > 0) return $"{n} " + SequenceOfNumbers(n - 1);
+    else return string.Empty;
+}
+
+Console.Write(SequenceOfNumbers(number));
