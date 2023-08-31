651f3e29b0c7e8677acc48cee46d4855fb487f54
axlerya <white_cat_02@mail.ru>
2023-08-31 21:06:15 +0500

    feat: add task 3
    add a solution to the third task

---
+++ task3/Program.cs
@@ -0,0 +1,13 @@
+﻿Console.WriteLine("Введите первое число");
+int numberOne = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите второе число");
+int numberTwo = Convert.ToInt32(Console.ReadLine());
+
+int AckermannFunction(int m, int n)
+{
+    if (m == 0) return n + 1;
+    else if (n == 0) return AckermannFunction(m - 1, 1);
+    else return AckermannFunction(m - 1, AckermannFunction(m, n - 1));
+}
+
+Console.WriteLine(AckermannFunction(numberOne, numberTwo));
