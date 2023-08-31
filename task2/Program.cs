feb7a65065077f03a99a69f368345227016ed75b
axlerya <white_cat_02@mail.ru>
2023-08-31 21:05:53 +0500

    feat: add task 2
    add a solution to the second task

---
+++ task2/Program.cs
@@ -0,0 +1,12 @@
+﻿Console.WriteLine("Введите первое число");
+int numberOne = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите второе число");
+int numberTwo = Convert.ToInt32(Console.ReadLine());
+
+int SequenceOfNumbers(int n, int m)
+{
+    if (n <= m) return n + SequenceOfNumbers(n + 1, m);
+    else return 0;
+}
+
+Console.WriteLine(SequenceOfNumbers(numberOne, numberTwo));
